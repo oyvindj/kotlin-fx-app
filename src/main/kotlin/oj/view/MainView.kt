837593acc95ae4e98c58dc9681aa194d70d1afe3
oyvindj <oyvindj@me.com>
2018-07-25 01:04:25 +0200

    minor changes

--- src/main/kotlin/oj/view/MainView.kt
+++ src/main/kotlin/oj/view/MainView.kt
@@ -6,8 +6,18 @@ import tornadofx.*
 class MainView : View("Hello TornadoFX") {
     override val root = hbox {
         addClass(Styles.body)
-        label(title) {
-            addClass(Styles.heading)
+        vbox {
+            label(title) {
+                addClass(Styles.heading)
+            }
+            button("Click Me") {
+                style {
+                    fontSize = 34.px
+                    minWidth = 100.px
+
+                }
+            }
+
         }
     }
 }
