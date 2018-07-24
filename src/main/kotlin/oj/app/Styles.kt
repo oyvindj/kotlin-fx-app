4313c2715e18f57b9e1a80d800e34f2c324e1f05
Oyvind Johansen <oyvindj@me.com>
2018-07-24 23:32:24 +0200

    minor change

--- src/main/kotlin/oj/app/Styles.kt
+++ src/main/kotlin/oj/app/Styles.kt
@@ -1,5 +1,6 @@
 package oj.app
 
+import javafx.scene.paint.Color
 import javafx.scene.text.FontWeight
 import tornadofx.Stylesheet
 import tornadofx.box
@@ -9,12 +10,16 @@ import tornadofx.px
 class Styles : Stylesheet() {
     companion object {
         val heading by cssclass()
+        val body by cssclass()
     }
 
     init {
+        body {
+            backgroundColor += Color.ORANGE
+        }
         label and heading {
             padding = box(10.px)
-            fontSize = 20.px
+            fontSize = 34.px
             fontWeight = FontWeight.BOLD
         }
     }
--- src/main/kotlin/oj/view/MainView.kt
+++ src/main/kotlin/oj/view/MainView.kt
@@ -5,6 +5,7 @@ import tornadofx.*
 
 class MainView : View("Hello TornadoFX") {
     override val root = hbox {
+        addClass(Styles.body)
         label(title) {
             addClass(Styles.heading)
         }
