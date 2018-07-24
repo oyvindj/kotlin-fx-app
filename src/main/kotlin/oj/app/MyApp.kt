ba52426437b1b24efaed3c8cb65e4108ecb72062
Oyvind Johansen <oyvindj@me.com>
2018-07-24 16:50:26 +0200

    minor change

---
+++ src/main/kotlin/oj/app/MyApp.kt
@@ -0,0 +1,6 @@
+package oj.app
+
+import oj.view.MainView
+import tornadofx.App
+
+class MyApp: App(MainView::class, Styles::class)
---
+++ src/main/kotlin/oj/view/MainView.kt
@@ -0,0 +1,12 @@
+package oj.view
+
+import oj.app.Styles
+import tornadofx.*
+
+class MainView : View("Hello TornadoFX") {
+    override val root = hbox {
+        label(title) {
+            addClass(Styles.heading)
+        }
+    }
+}
